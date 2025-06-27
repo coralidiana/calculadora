ba9010201567fa0574e295f7bab5e8b845cff61b
corali <dianalmanza20@gmail.com>
2025-06-27 19:57:38 -0400

    feat: cambios error de codigo

--- test_calculator.py
+++ test_calculator.py
@@ -11,7 +11,7 @@ def test_multiply():
     assert multiply(2, 3) == 6
 
 def test_divide():
-    assert divide(10, 2) == 6
+    assert divide(10, 2) == 5
 
 def test_divide_by_zero():
     with pytest.raises(ValueError):
